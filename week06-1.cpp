7d7ccb2e21e1ee9288a7b5eb9bf195611e04f38d
mufan0720 <114455943+mufan0720@users.noreply.github.com>
2022-10-11 11:49:55 +0800

    week06

---
+++ week06-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    ///for(int i=0; i<5; i++){
+    ///printf("i:%d\n", i);
+    ///}
+    for(int i=5; i>0; i--){
+        for(int j=0; j<1; j++){
+            printf("*");
+        }
+        printf("i:%d¬P¬P\n",i);
+    }
+}
