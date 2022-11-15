7cd7f2565b4c38facf58e31585ce166d59e23777
mufan0720 <chenmufan211@gmail.com>
2022-11-15 12:24:20 +0800

    week11

---
+++ week11/week11-1.cpp
@@ -0,0 +1,16 @@
+///三數最大值
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int max=a;
+    if(b>max) max = b;
+    if(c>max) max = c;
+
+    ///printf("%d %d %d", a, b, c);
+    printf("%d", max);
+
+}
+
