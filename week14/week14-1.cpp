c18bc7e05041961158d40fb4c7330c0549a612d4
mufan0720 <chenmufan211@gmail.com>
2022-12-06 11:59:09 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,16 @@
+///狡策:俱计跑计 int a; a琌俱计
+///狡策:ㄧΑ(量ウ)
+/// int sum( int a, int b); 
+
+#include <stdio.h>
+int sum( int a, int b);/// declare
+
+int main()
+{///ノぇ玡, 璶 declare ┪ ﹚竡 define
+    int ans = sum(2, 3); ///ㄏノ ㊣
+    printf("ans: %d\n", ans);
+}
+int sum( int a, int b) ///﹚竡 define
+{
+    return a + b;
+}///肚 確產
